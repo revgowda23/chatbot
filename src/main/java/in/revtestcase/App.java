2b876b0b6896fd4b4a2b9688b9affbcb562adb7d
revathi <revkumar230497@gmail.com>
2023-08-09 17:06:32 +0000

    initial project setup

---
+++ src/main/java/in/revtestcase/App.java
@@ -0,0 +1,13 @@
+package in.revtestcase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
